abb9fcdb4dd190726d0bc60b0e823c4448b29df1
michel <lug.limbe@gmail.com>
2025-02-27 10:02:38 +0000

    clean up app.py and model2.py

--- app.py
+++ app.py
@@ -3,15 +3,13 @@ from flask import Flask, render_template, request, redirect, url_for
 from models2 import db, OneTb, NTb
 
 
-#from sqlalchemy import create_engine
+
 #from sqlalchemy .orm import sessionmaker
 
 
 app = Flask(__name__)
 
-#engine = create_engine('sqlite:///your_database.db')
-#Session = sessionmaker(bind=engine)
-
+# if you want to change the database, change it on the line below
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app3.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -24,7 +22,7 @@ def create_tables():
 @app.route('/', methods=['GET', 'POST'])
 def index():
     tFieldNames = OneTb.get_field_names()
-    print (tFieldNames)
+    #print (tFieldNames)
     tFNoIds = []
     for tFieldName in tFieldNames:
         if tFieldName != 'id':
@@ -49,57 +47,40 @@ def index():
         # Create a new 1Tb record
         table = OneTb.__tablename__
         data = {tFNoId: request.form[tFNoId] for tFNoId in tFNoIds}
-        #insert_query = table.insert().values(**data)
-        #insert_query = OneTb.insert().values(**data)
         new_record = OneTb(**data)
         db.session.add(new_record)
-        '''
-        db.session.execute(insert_query)
-
-        name = request.form['name']
-        new_record = OneTb(name=name)
-        db.session.add(new_record)
-        '''
+        
         db.session.commit()
-
-        #nrelations = NTb.query.all()
-        #print ('nrelations')
-        #print (nrelations)
 
         return redirect(url_for('index'))
 
     # Fetch all 1Tb records
     records = OneTb.query.all()
 
-    ##nrelations = NTb.query.all()
-    print ('records')
-    print (records)
-    #return render_template('index.html',records=records,nrelations=nrelations,tnFieldNamesNoIds=tnFieldNamesNoIds,ntFieldNamesNoIds=ntFieldNamesNoIds)
-
-    #return render_template('index.html', records=records, tFieldNamesNoIds=tFieldNamesNoIds)
+    #print ('records')
+    #print (records)
+    
     return render_template('index.html', records=records, tFNoIds=tFNoIds)
-    #return render_template('nrelations.html', nrelations=nrelations, ntFieldNamesNoIds=ntFieldNamesNoIds)
-
+    
 
 
 
 @app.route('/nrelations/<int:one_tb_id>', methods=['GET', 'POST'])
 def nrelations(one_tb_id):
     fieldNames = NTb.get_field_names()
-    print (fieldNames)
+    #print (fieldNames)
     fieldNamesNoIds = []
     for fieldName in fieldNames:
         if fieldName != 'id':
            fieldNamesNoIds.append(fieldName)
-    print ('fieldNamesNoIds')
-    print (fieldNamesNoIds)
+    #print ('fieldNamesNoIds')
+    #print (fieldNamesNoIds)
     
     # list all relations 
     #nrelations = NTb.query.all()
     nrelations = NTb.query.filter(NTb.one_id == one_tb_id).all()
-    #nrelations = NTb.query.all()
-    print ('nrelations')
-    print (nrelations)
+    #print ('nrelations')
+    #print (nrelations)
     
     if request.method == 'POST':
         # Create a new nTb record
@@ -112,8 +93,7 @@ def nrelations(one_tb_id):
         db.session.commit()
         return redirect(url_for('index'))
      
-    #table_info = get_table_info('nTb')
-    #return render_template('nrelations.html', one_tb_id=one_tb_id, table_info=table_info, fieldNamesNoIds=fieldNamesNoIds )
+    
     return render_template('nrelations.html', one_tb_id=one_tb_id, fieldNamesNoIds=fieldNamesNoIds, nrelations=nrelations )
 
     
--- models2.py
+++ models2.py
@@ -5,10 +5,12 @@ db = SQLAlchemy()
 class OneTb(db.Model):
     __tablename__ = 'oneTb'
     id = db.Column(db.Integer, primary_key=True)
+    # from here zou can change, add or remove te fieldNames you to have 
     firstName = db.Column(db.String(50))
     lastName = db.Column(db.String(200))
     salary = db.Column(db.Integer)
 
+    # this function  return all fieldNames of this table in an array
     def get_field_names():
         oneTb_fields = [column.key for column in OneTb.__table__.columns]
         return oneTb_fields
@@ -19,25 +21,27 @@ class NTb(db.Model):
     __tablename__ = 'nTb'
     id = db.Column(db.Integer, primary_key=True)
     one_id = db.Column(db.Integer, db.ForeignKey('oneTb.id'))
+    # from here zou can change, add or remove te fieldNames you to have 
     value = db.Column(db.String(100))
     price = db.Column(db.Integer)
 
+    # this function  return all fieldNames of this table in an array
     def get_field_names():
         nTb_fields = [column.key for column in NTb.__table__.columns]
         return nTb_fields
  
-    #one = db.relationship('oneTb', backref=db.backref('nTb', lazy=True))
 
 
-# Beispiel zur Verwendung der Funktion
+# example how to use this function
+
 if __name__ == '__main__': 
 
-     
+    # it list all th fieldNames of OneTb       
     OneTbFieldNames = OneTb.get_field_names() 
     print ('OneTbFieldNames') 
     print (OneTbFieldNames) 
 
-
+    # it list all th fieldNames of NTb       
     NTbFieldNames = NTb.get_field_names() 
     print ('NTbFieldNames') 
     print (NTbFieldNames)
